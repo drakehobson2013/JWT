f0de1ce4130123abd46c6ac07931757ffbc7ab36
Drake Hobson <drakehobson2013@gmail.com>
2021-03-05 12:02:39 -0500

    switch to heroku Drake

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -14,6 +14,20 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password is required'],
         minlength: [6, 'Password minimum length is 6 characters'],
     },
+});
+
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created & saved', doc);
+  next();  
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function (next){
+    console.log('user about to be created and save', this);
+    const salt = bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
