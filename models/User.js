651f0015fbf469decec74165b030283a33a55623
Drake Hobson <drakehobson2013@gmail.com>
2021-03-10 12:03:42 -0500

    .

--- models/User.js
+++ models/User.js
@@ -16,19 +16,20 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+// fire a function before doc saved to db
+userSchema.pre('save', async function (next){
+    console.log('user about to be created and save', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
 // fire a function after doc saved to db
-userSchema.post('save', function (doc, next) {
+userSchema.post('save', async function (doc, next) {
     console.log('new user was created & saved', doc);
   next();  
 });
 
-// fire a function before doc saved to db
-userSchema.pre('save', async function (next){
-    console.log('user about to be created and save', this);
-    const salt = bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
 
 const User = mongoose.model('user', userSchema);
 
