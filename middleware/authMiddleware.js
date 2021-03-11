2aed535815067ad7830f3b33a266ee207680a39b
Drake Hobson <drakehobson2013@gmail.com>
2021-03-11 08:40:23 -0500

    16 and 17

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -22,4 +23,28 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req,res, next) => {
+    const token = req.cookies,jwt;
+
+    if (token){
+        jwt.verify(token, 'long ass secret', async (err, decodedToken) => {
+            if(err){
+                console.log(err.message);
+                next();
+            }
+            else{
+                console.log(decodedToken);
+                let user = await User.findByID(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })  
+    }
+    else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
